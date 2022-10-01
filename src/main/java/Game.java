85bc40674241eaa462f49b5e9071c669e824b357
Gabriel Coelho <up201902223@up.pt>
2022-10-01 13:16:53 +0100

    Reading KeyStrokes - Step 7 - all working

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,7 +10,10 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-    Screen screen;
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
+    private boolean gameCycleState;
 
     public Game(){
         try {
@@ -21,18 +26,46 @@ public class Game {
             screen.setCursorPosition(null);
             screen.startScreen();
             screen.doResizeIfNecessary();
+            gameCycleState = true;
         } catch(IOException e){
             e.printStackTrace();
         }
     }
 
+    private void processKey(KeyStroke key) throws IOException{
+        System.out.println(key);
+        if(key.getKeyType() == KeyType.ArrowLeft){
+            x -= 1;
+        }
+        if(key.getKeyType() == KeyType.ArrowRight){
+            x += 1;
+        }
+        if(key.getKeyType() == KeyType.ArrowUp){
+            y -= 1;
+        }
+        if(key.getKeyType() == KeyType.ArrowDown){
+            y += 1;
+        }
+
+        if(key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+            screen.close();
+        }
+        if(key.getKeyType() == KeyType.EOF){
+            gameCycleState = false;
+        }
+    }
+
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() throws IOException {
-        draw();
+        while(gameCycleState){
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+        }
     }
 }
